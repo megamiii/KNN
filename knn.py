a5edd34531cf216ae7e30fb0ae5aa0500c172e1d
damla <dam.la@snu.ac.kr>
2023-12-24 02:17:25 +0900

    Update KNN algorithm implementation and evaluation functions
    - Implemented `load_knn_data` function to import training and testing data from CSV files within the 'knn-dataset' directory.
    - Created `predict_knn` function that uses Euclidean distance to find the k nearest neighbors and predict the label for a given data point.
    - Developed `eval_knn` function to calculate the accuracy of the KNN classifier by comparing predicted labels against true labels.
    - Established `cross_validation_knn` function for performing k-fold cross-validation to find the optimal number of neighbors (K) for the classifier.
    - Added `plot_knn_accuracies` function to visualize the relationship between the number of neighbors and the accuracy of the model.
    - Structured the `main` function to tie all components together, performing data loading, hyperparameter tuning, result plotting, and test evaluation.

--- knn.py
+++ knn.py
@@ -27,7 +27,22 @@ def predict_knn(x, inputs, labels, k_neighbors):
     predicted_label = 0
     ########
     # TO DO:
+    # Extract numpy arrays from tuples if necessary
+    inputs = inputs[0] if isinstance(inputs, tuple) else inputs
+    labels = labels[0] if isinstance(labels, tuple) else labels
 
+    # Measure distances with the Euclidean norm (L2 norm)
+    distances = np.sqrt(((inputs - x)**2).sum(axis=1))
+    
+    # Get the indices of the k_neighbors smallest distances
+    knn_indices = distances.argsort()[:k_neighbors]
+    
+    # Get the labels of the nearest neighbors
+    knn_labels = labels[knn_indices]
+    
+    # Get the most frequent label among the nearest neighbors
+    (values, counts) = np.unique(knn_labels, return_counts=True)
+    predicted_label = values[np.argmax((counts))]
 
     ########
     return predicted_label
@@ -51,7 +66,29 @@ def eval_knn(inputs, labels, train_inputs, train_labels, k_neighbors):
     accuracy = 0
     ########
     # TO DO:
+    # Extract numpy arrays from tuples if necessary
+    inputs = inputs[0] if isinstance(inputs, tuple) else inputs
+    labels = labels[0] if isinstance(labels, tuple) else labels
+    train_inputs = train_inputs[0] if isinstance(train_inputs, tuple) else train_inputs
+    train_labels = train_labels[0] if isinstance(train_labels, tuple) else train_labels
 
+    # Initialize a counter for the number of correct predictions
+    correct_predictions = 0
+    
+    # Loop over each data point and its corresponding true label in the inputs and labels
+    for x, true_label in zip(inputs, labels):
+        # Reshape x to be a single data point
+        x_reshaped = x.reshape(1, -1)
+
+        # Predict the label
+        predicted_label = predict_knn(x_reshaped, train_inputs, train_labels, k_neighbors)
+        
+        # If the predicted label matches the true label, increment the counter
+        if predicted_label == true_label:
+            correct_predictions += 1
+            
+    # Calculate accuracy as the ratio of correct predictions to the total number of predictions
+    accuracy = correct_predictions / len(labels)
 
     ########
     return accuracy
@@ -76,7 +113,41 @@ def cross_validation_knn(k_folds, hyperparameters, inputs, labels):
     accuracies = np.zeros(len(hyperparameters))
     ########
     # TO DO:
+    # Extract numpy arrays from tuples if necessary
+    inputs = inputs[0] if isinstance(inputs, tuple) else inputs
+    labels = labels[0] if isinstance(labels, tuple) else labels
 
+    # Calculate the size of each fold
+    fold_size = len(inputs) // k_folds
+    
+    # Iterate over each hyperparameter (k_neighbors)
+    for k in hyperparameters:
+        fold_accuracies = []
+        
+        # Perform k-fold cross-validation
+        for i in range(k_folds):
+            # Determine the start and end indices of the validation set
+            val_start = i * fold_size
+            val_end = (i + 1) * fold_size
+            
+            # Split the dataset into validation and training sets
+            val_inputs = inputs[val_start:val_end]
+            val_labels = labels[val_start:val_end]
+            train_inputs = np.vstack([inputs[:val_start], inputs[val_end:]])
+            train_labels = np.concatenate([labels[:val_start], labels[val_end:]])
+            
+            # Evaluate the accuracy on the validation set
+            fold_accuracy = eval_knn(val_inputs, val_labels, train_inputs, train_labels, k)
+            fold_accuracies.append(fold_accuracy)
+        
+        # Calculate the average accuracy across all folds for the current hyperparameter
+        avg_accuracy = np.mean(fold_accuracies)
+        accuracies[hyperparameters.index(k)] = avg_accuracy
+        
+        # Update best_hyperparam and best_accuracy if current hyperparameter outperforms previous ones
+        if avg_accuracy > best_accuracy:
+            best_accuracy = avg_accuracy
+            best_hyperparam = k
 
     ########
     return best_hyperparam, best_accuracy, accuracies
